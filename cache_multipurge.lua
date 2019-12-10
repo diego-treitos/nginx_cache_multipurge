d28f6613058cdbe71544981e1f948f5772096b9c
Diego Blanco <diego.blanco@treitos.com>
2019-12-10 13:10:52 +0100

    Several improvements, still untested

--- cache_multipurge.lua
+++ cache_multipurge.lua
@@ -13,8 +13,12 @@ local md5 = require 'md5'
 ---------------------------------- globals -------------------------------------
 --------------------------------------------------------------------------------
 --
-local cmp_cache_key = ngx.var.cmp_cache_key -- i.e. $proxy_cache_key
-local cmp_cache_path = ngx.var.cmp_cache_path -- i.e. /var/www/cache
+-- Value of the cache_key parameter. i.e. $scheme$host$uri
+local cmp_cache_key = ngx.var.cmp_cache_key
+-- Path where the cache files are stored. i.e. /var/www/cache
+local cmp_cache_path = ngx.var.cmp_cache_path
+-- Initial segment of the URL to be stripped to get the URL to purge. i.e. /purge/dynamic
+local cmp_cache_strip = ngx.var.cmp_cache_strip or ''
 
 
 --------------------------------------------------------------------------------
@@ -35,22 +39,44 @@ function validate( uri )
   if ngx.re.match( uri , valid_url_re, 'ijo') then return true else return false end
 end
 
-
-function purge_all()
-  os.execute( "rm -rd '"..cmp_cache_path.."/*'" )
+---------------------------------------- replace string once without patterns --
+function string_replace( target, match, replace )
+  local match_s, match_e = target:find( match )
+  if match_s then
+    match_s = match_s-1
+    match_e = match_e+1
+    return target:sub( 1, match_s ) .. replace .. target:sub( match_e )
+  else
+    return target
+  end
 end
 
+
+--------------------------------------------------------------------------------
+------------------------------ purge functions ---------------------------------
+--------------------------------------------------------------------------------
+--
+----------------------------------------------------------- purge all entries --
+function purge_all()
+  -- os.execute( "rm -rd '"..cmp_cache_path.."/*'" )
+  os.execute( "echo '"..cmp_cache_path.."/*' >> /tmp/nginx_purge.log" )
+end
+
+------------------------------------------------------ purge matching entries --
 function purge_multi( safe_uri )
   -- escape special characters for grep
   local cache_key_re = string.gsub( cmp_cache_key, "([%.%[%]])", "\\%1" )
-  local cache_key_re = string.gsub( cache_key_re, ngx.var.request_uri, safe_uri..".*" )
+  local cache_key_re = string_replace( cache_key_re, cmp_uri, safe_uri..".*" )
 
-  os.execute( "grep -Raslm1 '^KEY: "..cache_key_re.."' "..cmp_cache_path.." | xargs -r rm -f" )
+  --os.execute( "grep -Raslm1 '^KEY: "..cache_key_re.."' "..cmp_cache_path.." | xargs -r rm -f" )
+  os.execute( "grep -Raslm1 '^KEY: "..cache_key_re.."' "..cmp_cache_path.." >> /tmp/nginx_purge.log 2>&1" )
 end
 
-function purge_one( uri )
+-------------------------------------------------------- purge specific entry --
+function purge_one()
   local cache_key_md5 = md5.sumhexa( cmp_cache_key )
-  os.execute( "find '"..cmp_cache_path.."' -name '"..cache_key_md5.."' -type f -exec rm {} +" )
+  -- os.execute( "find '"..cmp_cache_path.."' -name '"..cache_key_md5.."' -type f -exec rm {} +" )
+  os.execute( "find '"..cmp_cache_path.."' -name '"..cache_key_md5.."' -type f >> /tmp/nginx_purge.log 2>&1" )
 end
 
 --------------------------------------------------------------------------------
@@ -61,17 +87,25 @@ end
 if ngx.var.request_uri == '/*' then
   purge_all()
 else
+  ------ FIXES
+  -- remove tailing slash from cmp_cache_strip
+  cmp_cache_strip = string.gsub( cmp_cache_strip, '/$', '' )
+  -- url to be purged
+  local cmp_uri = string.gsub( ngx.var.request_uri, "^"..cmp_cache_strip, "" )
+  -- change the cache key to use the right url
+  cmp_cache_key = string_replace( cmp_cache_key, ngx.var.request_uri, cmp_uri )
+
   -- check if last character of the request_uri is a *
-  if string.find( string.reverse( ngx.var.request_uri ), '*', 1, 1 ) then
+  if string.find( string.reverse( cmp_uri ), '*', 1, 1 ) then
     -- uri is request_uri without the trailing *
-    local uri = string.sub( ngx.var.request_uri, 1, string.len( ngx.var.request_uri ) - 1 )
+    local uri = string.gsub( cmp_uri, '%*$', '' )
     if validate( uri ) then
       purge_multi( uri )
     else
       ngx.exit( ngx.HTTP_BAD_REQUEST )
     end
   else
-    purge_one( ngx.var.request_uri )
+    purge_one()
   end
 end
 
