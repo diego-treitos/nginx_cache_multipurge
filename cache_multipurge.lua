ecff8a41d52151dd1ebcc9f270abce8c4599d163
Diego Blanco <diego.blanco@treitos.com>
2019-12-11 12:03:38 +0100

    Fixes and improved security. Retested and working.

--- cache_multipurge.lua
+++ cache_multipurge.lua
@@ -3,8 +3,7 @@
 -- Diego Blanco <diego.blanco@treitos.com>
 
 -- LIMITATIONS:
---   * cache_key must contain request_uri
---   * urls with $ ' " | ` characters will not work
+--   * cache_key must contain $request_uri (which totally should)
 
 local md5 = require 'md5'
 
@@ -13,7 +12,7 @@ local md5 = require 'md5'
 --------------------------------------------------------------------------------
 --
 -- Variable to control wether to run or not. 
-local cmp_run_if = ngx.var.cmp_run_if
+local cmp_run_if = ngx.var.cmp_run_if or ''
 -- Value of the cache_key variable. i.e. $proxy_cache_key
 local cmp_cache_key = ngx.var.cmp_cache_key
 -- Path where the cache files are stored. i.e. /var/www/cache
@@ -23,7 +22,7 @@ local cmp_cache_strip = ngx.var.cmp_cache_strip or ''
 
 ------ patch global variables
 -- check if we won't purge
-if not cmp_run_if then ngx.exit( ngx.OK ) end
+if cmp_run_if == '' or cmp_run_if == '0' then ngx.exit( ngx.OK ) end
 -- remove tailing slash from cmp_cache_strip
 cmp_cache_strip = cmp_cache_strip:gsub( '/$', '' )
 -- url to be purged
@@ -36,18 +35,10 @@ cmp_cache_key = cmp_cache_key:gsub( ngx.var.request_uri:gsub("%p", "%%%1"), cmp_
 ---------------------------------- helpers -------------------------------------
 --------------------------------------------------------------------------------
 --
------------------------------------------------------- check if a file exists --
-function file_exists( name )
-  local f=io.open( name, 'r' )
-  if f~=nil then io.close(f) return true else return false end
-end
-
------------------------------------------------- check if uri is a valid path --
-function validate( uri )
-  -- selection of characters allowed in URL to prevent RCE
-  -- NOT allowed: ' " $ ` | 
-  local valid_url_re = "^/[a-z0-9\\./_~!&\\(\\)\\*\\+,;=:%@\\-]+$"
-  if ngx.re.match( uri , valid_url_re, 'ijo') then return true else return false end
+--------------------- sanitize string to be used as a shell command parameter --
+function safe_shell_command_param( string_with_user_input )
+  -- prevent command injection
+  return "'"..string_with_user_input:gsub( "%'", "'\"'\"'" ).."'"
 end
 
 
@@ -61,12 +52,13 @@ function purge_all()
 end
 
 ------------------------------------------------------ purge matching entries --
-function purge_multi( safe_uri )
+function purge_multi( uri )
   -- escape special characters for grep
   local cache_key_re = cmp_cache_key:gsub( "([%.%[%]])", "\\%1" )
-  local cache_key_re = cache_key_re:gsub( cmp_uri:gsub("%p","%%%1"), safe_uri..".*" )
+  local cache_key_re = cache_key_re:gsub( cmp_uri:gsub("%p","%%%1"), uri..".*" )
+  local safe_grep_param = safe_shell_command_param( "^KEY: "..cache_key_re )
 
-  os.execute( "grep -Raslm1 '^KEY: "..cache_key_re.."' "..cmp_cache_path.." | xargs -r rm -f" )
+  os.execute( "grep -Raslm1  "..safe_grep_param.." "..cmp_cache_path.." | xargs -r rm -f" )
 end
 
 -------------------------------------------------------- purge specific entry --
@@ -86,14 +78,10 @@ if string.find( string.reverse( cmp_uri ), '*', 1, 1 ) then
   else
     -- uri is request_uri without the trailing *
     local uri = string.gsub( cmp_uri, '%*$', '' )
-    if validate( uri ) then
-      purge_multi( uri )
-    else
-      ngx.exit( ngx.HTTP_BAD_REQUEST )
-    end
+    purge_multi( uri )
   end
 else
-    purge_one()
+  purge_one()
 end
 
 ngx.exit( ngx.HTTP_NO_CONTENT ) -- Status code if everything works
